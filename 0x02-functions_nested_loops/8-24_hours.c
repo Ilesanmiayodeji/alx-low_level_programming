3fe0cb8cc8b7a13d32945e5c024e2f15e844ab14
Ilesanmiayodeji <ilayodeji@gmail.com>
2022-09-13 08:51:11 -0700

    I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,7 +5,7 @@
  * h = hour, m = minutes
  * / 10 allows second digit to rotate
  * for loop breaks before passing 24:00
- * Return: 24 hour clock line by line
+ * 24 hour clock line by line
  * Description: function prints every minute in one day
  * Return: Nothing
  */
