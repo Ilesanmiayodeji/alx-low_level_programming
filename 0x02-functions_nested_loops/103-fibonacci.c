4e2e5d6fb1663b1998b641201de99b2d28677f17
Ilesanmiayodeji <ilayodeji@gmail.com>
2022-09-13 10:57:29 -0700

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -14,11 +14,13 @@ int main(void)
 
 	fibonacci[0] = 1;
 	fibonacci[1] = 2;
+
 	for (i = 2; i < 50; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
 		if ((fibonacci[i] % 2) == 0 && fibonacci[i] < 4000000)
 			sum += fibonacci[i];
 	}
+	
 	printf("%ld\n", sum);
 }
