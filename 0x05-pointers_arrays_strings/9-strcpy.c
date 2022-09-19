d74a12dd8ef57a4dcadb54f7dbd55138235e9fbc
Ilesanmiayodeji <ilayodeji@gmail.com>
2022-09-19 08:00:17 -0700

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,14 +9,11 @@
  * Return: pointer to dest
  */
 
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
-	int index = 0;
+	char *aux = dest;
 
-	while (src[index])
-	{
-		dest[index] = src[index];
-		index++;
-	}
-	return (dest);
+	while (*src)
+		*dest++ = *src++;
+	return (aux);
 }
