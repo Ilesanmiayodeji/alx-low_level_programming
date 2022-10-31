569107f152cdccd4aae0fedfa63606af21e19479
Ilesanmiayodeji <ilayodeji@gmail.com>
2022-10-31 08:15:55 -0700

    Update on task 4

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,15 +1,13 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
+#ifndef __MAIN_H
+#define __MAIN_H
 
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <sys/uio.h>
-#include <unistd.h>
 #include <fcntl.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* __MAIN_H__ */
+#endif /* __MAIN_H */
